27af2aab6d1959189fd07f8ab7e578221ac46348
Vlad <vlad.nagaev.vn@gmail.com>
2017-11-02 18:28:37 +0200

    model logic created

--- Task2/src/ua/training/model/Game.java
+++ Task2/src/ua/training/model/Game.java
@@ -9,9 +9,20 @@
 package ua.training.model;
 
 public class Game {
-
     private int number;
 
+    public int getNumber() {
+        return number;
+    }
 
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int compareNumber(int number) {
+        if(number == this.number) return 0;
+        int dif = number - this.number;
+        return dif/Math.abs(dif);
+    }
 
 }
